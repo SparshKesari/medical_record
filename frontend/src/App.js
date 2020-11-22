4d3e1ce90378099edd5305bc58913c32a168909b
Ashutosh Mahapatra <amahapatra13@gmail.com>
2020-11-22 18:53:52 +0530

    connected the flask and react servers

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Login from './Component/Login'
+import { Container, Col, Row } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Component/Signup';
+import Landing_Page from './Pages/Landing_Page';
+import NavBar from './Component/Navigation';
+import Api from './Component/Api'
+
+function App() {
+  return (
+      <>
+        <NavBar/>
+        <Switch>
+            <Route exact path='/' component={Landing_Page}></Route>
+            <Route exact path='/login' component={Login}></Route>
+            <Route path='/signup' component={Signup} ></Route>
+            <Route path='/api' component={Api} ></Route>
+        </Switch>
+      </>
+  );
+}
+
+export default App;
