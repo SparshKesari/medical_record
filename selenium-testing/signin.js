2441f9118667b6dd4aa920e4f00d8119f9918d21
Ashutosh Mahapatra <amahapatra13@gmail.com>
2020-12-29 10:11:55 +0530

    added the test cases

---
+++ selenium-testing/signin.js
@@ -0,0 +1,83 @@
+let credentials = require("./credentials.json"); 
+
+for(var i = 0; i<credentials.length; i++){
+
+
+    require("chromedriver"); 
+    
+
+    let swd = require("selenium-webdriver"); 
+    let browser = new swd.Builder(); 
+    let tab = browser.forBrowser("chrome").build(); 
+    
+    let {email, pass } = credentials[i]; 
+
+    
+
+    let tabToOpen = 
+        tab.get("http://localhost:3000/login"); 
+    tabToOpen 
+        .then(function () { 
+    
+    
+            let findTimeOutP = 
+                tab.manage().setTimeouts({ 
+                    implicit: 10000,  
+                }); 
+            return findTimeOutP; 
+        }) 
+        .then(function () { 
+    
+
+            let promiseUsernameBox = 
+                tab.findElement(swd.By.css("#formBasicEmail")); 
+            return promiseUsernameBox; 
+        }) 
+        .then(function (usernameBox) { 
+    
+
+            let promiseFillUsername = 
+                usernameBox.sendKeys(email); 
+            return promiseFillUsername; 
+        }) 
+        .then(function () { 
+            console.log( 
+                "Username entered successfully in"
+            ); 
+    
+
+            let promisePasswordBox = 
+                tab.findElement(swd.By.css("#formBasicPassword")); 
+            return promisePasswordBox; 
+        }) 
+        .then(function (passwordBox) { 
+    
+
+            let promiseFillPassword = 
+                passwordBox.sendKeys(pass); 
+            return promiseFillPassword; 
+        }) 
+        .then(function () { 
+            console.log( 
+                "Password entered successfully in"
+            ); 
+    
+
+            let promiseSignInBtn = tab.findElement( 
+                swd.By.css(".mb-3.btn.btn-primary") 
+            ); 
+            return promiseSignInBtn; 
+        }) 
+        .then(function (signInBtn) { 
+
+            let promiseClickSignIn = signInBtn.click(); 
+            return promiseClickSignIn; 
+        }) 
+        .then(function () { 
+            console.log("Successfully signed");
+        }) 
+        .catch(function (err) { 
+            console.log("Error ", err, " occurred!"); 
+        }); 
+
+}
